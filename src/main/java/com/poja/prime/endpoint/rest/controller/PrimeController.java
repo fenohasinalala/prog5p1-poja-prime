7eb24dcddbfd6d84d6fc9ceb3957c349187dadb0
Lova Fenohasina RAFANOMEZANTSOA <fenohasinalala@gmail.com>
2023-12-05 00:14:18 +0300

    feat: add GET /new-prime

--- src/main/java/com/poja/prime/endpoint/rest/controller/PrimeController.java
+++ src/main/java/com/poja/prime/endpoint/rest/controller/PrimeController.java
@@ -1,4 +1,19 @@
 package com.poja.prime.endpoint.rest.controller;
 
+import com.poja.prime.endpoint.rest.service.PrimeService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigInteger;
+
+@RestController
+@AllArgsConstructor
 public class PrimeController {
+    private final PrimeService service;
+
+    @GetMapping(value = "/new-prime")
+    public String generateNewPrime(){
+        return service.generateNewPrime().toString();
+    }
 }
--- src/main/java/com/poja/prime/endpoint/rest/service/PrimeService.java
+++ src/main/java/com/poja/prime/endpoint/rest/service/PrimeService.java
@@ -1,4 +1,17 @@
 package com.poja.prime.endpoint.rest.service;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigInteger;
+import java.util.Random;
+
+@Service
+@AllArgsConstructor
 public class PrimeService {
+    public BigInteger generateNewPrime() {
+        int bitLength = 10_000;
+        Random randomParameter = new Random();
+        return BigInteger.probablePrime(bitLength,randomParameter);
+    }
 }
