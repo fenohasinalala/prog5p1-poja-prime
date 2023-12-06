5056250af97215168028a54c9f3a26f70e94f17f
Lova Fenohasina RAFANOMEZANTSOA <fenohasinalala@gmail.com>
2023-12-06 09:33:32 +0300

    fix: lambda memory to 512 and bitNumber to 1_000

--- src/main/java/com/poja/prime/service/PrimeService.java
+++ src/main/java/com/poja/prime/service/PrimeService.java
@@ -15,7 +15,7 @@ public class PrimeService {
 
     private final PrimeNumberRepository repository;
     public String generateNewPrime() {
-        int bitLength = 3_000;
+        int bitLength = 1_000;
         Random randomParameter = new Random();
         String generated = BigInteger.probablePrime(bitLength, randomParameter).toString();
         return save(generated).getValue();
